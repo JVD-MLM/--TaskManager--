1abdd3a0fe1823f6a343e2b095bc3ee1cb0aec03
JVD <JVD>
2025-12-06 08:47:11 +0330

    .

--- Application/Utilities/PersianDate.cs
+++ Application/Utilities/PersianDate.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace TaskManager.Application.Utilities;
 
@@ -9,15 +10,18 @@ public static class PersianDate
 {
     public static string ToPersianDateTime(this DateTime date) // برای DateTime
     {
+        var iran = TimeZoneInfo.FindSystemTimeZoneById("Iran Standard Time");
+        var local = TimeZoneInfo.ConvertTimeFromUtc(date, iran);
+
         var pc = new PersianCalendar();
 
-        var year = pc.GetYear(date);
-        var month = pc.GetMonth(date);
-        var day = pc.GetDayOfMonth(date);
+        var year = pc.GetYear(local);
+        var month = pc.GetMonth(local);
+        var day = pc.GetDayOfMonth(local);
 
-        var hour = pc.GetHour(date);
-        var minute = pc.GetMinute(date);
-        var second = pc.GetSecond(date);
+        var hour = pc.GetHour(local);
+        var minute = pc.GetMinute(local);
+        var second = pc.GetSecond(local);
 
         return $"{year:0000}/{month:00}/{day:00} {hour:00}:{minute:00}:{second:00}";
     }
